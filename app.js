a5c611b49d8cf1fc8f555292a7f71a16d109a1f9
Behrooz Shahin <shahin.behrooz@gmail.com>
2025-03-01 17:49:16 -0500

    port changed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const server = express();
 server.get('/',(req,res)=>root(req,res));
-server.listen(5567,'0.0.0.0');
+server.listen(8080,'0.0.0.0');
+
 function root(req,res){
     res.send('Wlcome to paradise');
 }
